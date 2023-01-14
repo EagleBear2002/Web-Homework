35ffe29b492f760093f457b48cde05313cf8da8a
EagleBear <377568710@qq.com>
2023-01-14 15:58:32 +0800

    fix lab5

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-app.use(cors())
+app.use(cors());
 
 const mysql = require('mysql2');
 const connection = mysql.createConnection({
